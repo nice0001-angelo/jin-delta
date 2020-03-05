f1a654f54747d90ecc7228e35daaac1dbf4818e8
nice0001-angelo <nice0001@gmail.com>
2020-03-05 00:14:59 -0500

    update JUNIT Test

--- src/main/java/net/jin/board/persistence/BoardRepository.java
+++ src/main/java/net/jin/board/persistence/BoardRepository.java
@@ -1,13 +1,15 @@
 package net.jin.board.persistence;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import net.jin.board.domain.Board;
 
-public interface BoardRepository extends JpaRepository<Board, Long>{
-	@Query("SELECT b FROM Board b")
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+	@Query("SELECT b FROM Board b")  
 	Page<Board> getBoardList(Pageable pageable);
 }
