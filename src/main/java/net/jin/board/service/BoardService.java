fb002e8ff4313467a375ca2bc08ee908802f56c9
nice0001-angelo <nice0001@gmail.com>
2020-03-08 11:11:26 -0400

    update BoardService

--- src/main/java/net/jin/board/service/BoardService.java
+++ src/main/java/net/jin/board/service/BoardService.java
@@ -1,5 +1,18 @@
 package net.jin.board.service;
 
-public interface BoardService {
+import org.springframework.data.domain.Page;
 
+import net.jin.board.domain.Board;
+
+public interface BoardService {
+	
+	void insertBoard(Board board);
+	
+	void updateBoard(Board board);
+	
+	void deleteBoard(Board board);
+	
+	Board getBoard(Board board);
+	
+	Page<Board> getBoardList(Board board);
 }
