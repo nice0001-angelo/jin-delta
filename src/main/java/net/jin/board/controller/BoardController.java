0af4f49866b4a711e73f90287f22b9194a263f9d
nice0001-angelo <nice0001@gmail.com>
2020-03-13 05:50:27 -0400

    update BoardController

--- src/main/java/net/jin/board/controller/BoardController.java
+++ src/main/java/net/jin/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.jin.board.domain.Board;
@@ -18,7 +19,6 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 
-
 	@RequestMapping("/getBoardList")
 	public String getBoardList(Model model, Board board) {
 		Page<Board> boardList = boardService.getBoardList(board);
@@ -26,4 +26,10 @@ public class BoardController {
 		return "board/getBoardList";
 	}
 
+	@GetMapping("/getBoard")
+	public String getBoard(Model model, Board board) {
+		Board getboard = boardService.getBoard(board);
+		model.addAttribute("board", getboard);
+		return "board/getBoard";
+	}
 }
