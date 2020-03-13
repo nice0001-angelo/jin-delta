c67bb3f6ce26ab27db7b03c167042e6d94eba433
nice0001-angelo <nice0001@gmail.com>
2020-03-13 04:14:15 -0400

    update CrudRepository

--- src/main/java/net/jin/board/persistence/BoardRepository.java
+++ src/main/java/net/jin/board/persistence/BoardRepository.java
@@ -3,12 +3,12 @@ package net.jin.board.persistence;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import net.jin.board.domain.Board;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends CrudRepository<Board, Long> {
 
 	@Query("SELECT b FROM Board b")  
 	Page<Board> getBoardList(Pageable pageable);
--- src/main/java/net/jin/board/persistence/MemberRepository.java
+++ src/main/java/net/jin/board/persistence/MemberRepository.java
@@ -1,8 +1,8 @@
 package net.jin.board.persistence;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import net.jin.board.domain.Member;
 
-public interface MemberRepository extends JpaRepository<Member, String>{
+public interface MemberRepository extends CrudRepository<Member, String>{
 }
