a675d7676d424e20142cff10e8668319a98be64a
nice0001-angelo <nice0001@gmail.com>
2020-03-14 13:10:00 -0400

    update SecurityConfig

--- src/main/java/net/jin/board/security/SecurityConfig.java
+++ src/main/java/net/jin/board/security/SecurityConfig.java
@@ -1,8 +1,20 @@
 package net.jin.board.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private SecurityUserDetailsService securityUserDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		httpSecurity.userDetailsService(securityUserDetailsService);
+		
+		httpSecurity.authorizeRequests().antMatchers("/","/system/**").permitAll();
+	}
 
 }
