5ffbd7f16408c425b02183e61aaded9001ad5672
nice0001-angelo <nice0001@gmail.com>
2020-03-16 23:03:40 -0400

    update test

--- src/test/java/net/jin/BoardRepositoryTest.java
+++ src/test/java/net/jin/BoardRepositoryTest.java
@@ -4,9 +4,11 @@ package net.jin;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import net.jin.board.domain.Board;
 import net.jin.board.domain.Member;
+import net.jin.board.domain.Role;
 import net.jin.board.persistence.BoardRepository;
 import net.jin.board.persistence.MemberRepository;
 
@@ -19,55 +21,71 @@ public class BoardRepositoryTest {
 	@Autowired
 	private BoardRepository boardRepository;
 
-	/*@Test
-	 * public void testInsert() { Member member1 = new Member();
-	 * member1.setId("member"); member1.setPassword("1234");
-	 * member1.setName("Angelo"); member1.setRole(Role.ROLE_MEMBER);
-	 * member1.setEnabled(true); memberRepository.save(member1);
-	 * 
-	 * Member member2 = new Member(); member2.setId("admin");
-	 * member2.setPassword("1234"); member2.setName("Jee");
-	 * member2.setRole(Role.ROLE_ADMIN); member2.setEnabled(true);
-	 * memberRepository.save(member2);
-	 * 
-	 * for (int i = 1; i <= 13; i++) { Board board = new Board();
-	 * board.setMember(member1); board.setTitle(member1.getName() + "가 등록한 게시글" +
-	 * i); board.setContent(member1.getName() + "가 등록한 게시글" + i);
-	 * boardRepository.save(board); }
-	 * 
-	 * for (int i = 1; i <= 3; i++) { Board board = new Board();
-	 * board.setMember(member2); board.setTitle(member2.getName() + "가 등록한 게시글" +
-	 * i); board.setContent(member2.getName() + "가 등록한 게시글" + i);
-	 * boardRepository.save(board); } }
-	 */
-	
-	/*
-	 * @Test public void testGetBoard() {
-	 * 
-	 * Board board = boardRepository.findById(1L).get();
-	 * 
-	 * System.out.println("[" + board.getSeq() + "번 게시글 상세정보 ]");
-	 * System.out.println("제목\t : " + board.getTitle());
-	 * System.out.println("작성자\t : " + board.getMember().getName());
-	 * System.out.println("내용\t : " + board.getContent());
-	 * System.out.println("작성일\t : " + board.getCreateDate());
-	 * System.out.println("조회수\t : " + board.getCnt()); }
-	 */
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Test
+	public void testInsert() {
+		Member member1 = new Member();
+		member1.setId("member");
+		member1.setPassword(passwordEncoder.encode("1234"));
+		member1.setName("Angelo");
+		member1.setRole(Role.ROLE_MEMBER);
+		member1.setEnabled(true);
+		memberRepository.save(member1);
+
+		Member member2 = new Member();
+		member2.setId("admin");
+		member2.setPassword(passwordEncoder.encode("1234"));
+		member2.setName("Jee");
+		member2.setRole(Role.ROLE_ADMIN);
+		member2.setEnabled(true);
+		memberRepository.save(member2);
+
+		for (int i = 1; i <= 13; i++) {
+			Board board = new Board();
+			board.setMember(member1);
+			board.setTitle(member1.getName() + "가 등록한 게시글" + i);
+			board.setContent(member1.getName() + "가 등록한 게시글" + i);
+			boardRepository.save(board);
+		}
+
+		for (int i = 1; i <= 3; i++) {
+			Board board = new Board();
+			board.setMember(member2);
+			board.setTitle(member2.getName() + "가 등록한 게시글" + i);
+			board.setContent(member2.getName() + "가 등록한 게시글" + i);
+			boardRepository.save(board);
+		}
+	}
+
+	@Test
+	public void testGetBoard() {
+
+		Board board = boardRepository.findById(1L).get();
+
+		System.out.println("[" + board.getSeq() + "번 게시글 상세정보 ]");
+		System.out.println("제목\t : " + board.getTitle());
+		System.out.println("작성자\t : " + board.getMember().getName());
+		System.out.println("내용\t : " + board.getContent());
+		System.out.println("작성일\t : " + board.getCreateDate());
+		System.out.println("조회수\t : " + board.getCnt());
+	}
+
 	@Test
 	public void testGetBoardList() {
 		Member member = memberRepository.findById("member").get();
-		
+
 		System.out.println("[ " + member.getName() + "가 등록한 게시글 ]");
 		System.out.println("[ " + member.getPassword() + "가 등록한 게시글 ]");
 		System.out.println("-----> " + member.getBoardList().toString());
 		try {
-					for (Board board : member.getBoardList()) {
-			System.out.println("-----> " + board.toString());
-		}
+			for (Board board : member.getBoardList()) {
+				System.out.println("-----> " + board.toString());
+			}
 
 			/* System.out.println("[ " + member.getBoardList().size() + " 사이즈]"); */
-		}catch (Exception e){
+		} catch (Exception e) {
 			System.out.println("e.getMessage() : " + e.getMessage());
 		}
 	}
