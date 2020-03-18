d2cf3ab1072b14b031ab0a6467c0a1fb87020b9e
nice0001-angelo <nice0001@gmail.com>
2020-03-17 21:34:58 -0400

    update for dynamic sql

--- src/main/java/net/jin/board/persistence/BoardRepository.java
+++ src/main/java/net/jin/board/persistence/BoardRepository.java
@@ -4,11 +4,12 @@ package net.jin.board.persistence;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import net.jin.board.domain.Board;
 
-public interface BoardRepository extends CrudRepository<Board, Long> {
+public interface BoardRepository extends CrudRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
 
 	@Query("SELECT b FROM Board b")  
 	Page<Board> getBoardList(Pageable pageable);
