2347757633ce7871d516a965dfca5b03ab22e528
nice0001-angelo <nice0001@gmail.com>
2020-03-21 14:29:27 -0400

    update about

--- src/main/java/net/jin/controller/HomeController.java
+++ src/main/java/net/jin/controller/HomeController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 	
-	@GetMapping("")
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
 
+	@GetMapping("/about")
+	public String about() {
+		return "/text/about";
+	}
 }
